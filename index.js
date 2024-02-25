6812ab6d508749f8712ef0f92876ddbc915a00db
lakim dahal <lakimdahal1@gmail.com>
2024-02-25 09:31:36 +0545

    html form recap

---
+++ index.js
@@ -0,0 +1,21 @@
+const getSlider = () => {
+  const age = document.getElementById("age").value;
+  document.getElementById("rangeVal").innerHTML = age;
+};
+
+const getGender = () => {
+  const btns = document.querySelectorAll('input[name="gender"]');
+  let answer;
+  for (const btn of btns) {
+    if (btn.checked) {
+      answer = btn.value;
+      break;
+    }
+  }
+  document.getElementById("gender").innerHTML = answer;
+};
+
+const getName=()=>{
+    const val= document.querySelector('input[name="name"]').value;
+    document.getElementById("nameVal").innerHTML=val;
+}
